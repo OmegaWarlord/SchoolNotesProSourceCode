983b74744d4afa3e7dbded51e05fa46dda89ad43
OmegaWarlord <60246471+OmegaWarlord@users.noreply.github.com>
2020-03-06 10:05:33 -0600

    Update School Notes Helper Pro TB#00002.py

--- Test Builds/School Notes Helper Pro TB#00002.py
+++ Test Builds/School Notes Helper Pro TB#00002.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-from datetime import *
+import datetime
 import easygui as e #Datetime for updating label, easygui for simple boxes
 import os
 currenttime = None
 CurClass = None
-weekday = now().strftime("%A")
+weekday = datetime.now().strftime("%A")
 winopacity = []
 homeworklist = []
 homeworkdue = []
